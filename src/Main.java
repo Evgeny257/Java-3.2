f0137fc35388580ce323e5a677decd5e07dd4faf
Eugene <galanin.zhenia@yandex.ru>
2023-03-31 00:03:54 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int index = service.calculate(1.87, 98);
+
+        System.out.println("Your Bmi index is " + index);
+    }
+}
